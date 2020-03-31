a3aaae03e69baea911a5e8c80d1d9ba41088789d
gox <gox.roche@gmail.com>
2020-03-31 16:56:08 +0200

    Weapon choice

--- js/at-data.js
+++ js/at-data.js
@@ -1,134 +1,148 @@
 const weapons = {
-    warlord: [
-        {
-            name: "Apocalypse Missile Launchers",
-            type: "Carapace",
-            cost: 15
-        },
-        {
-            name: "Paired Gatling Blasters",
-            type: "Carapace",
-            cost: 30
-        },
-        {
-            name: "Paired Laser Blasters",
-            type: "Carapace",
-            cost: 50
-        },
-        {
-            name: "Paired Turbo Laser Destructors",
-            type: "Carapace",
-            cost: 35
-        },
-        {
-            name: "Vulcan Mega-bolter Array",
-            type: "Carapace",
-            cost: 20
-        },
-        {
-            name: "Plasma Blastgun",
-            type: "Arm",
-            cost: 30
-        },
-        {
-            name: "Arioch Titan Power Claw",
-            type: "Arm",
-            cost: 25
-        },
-        {
-            name: "Belicosa Volcano Cannon",
-            type: "Arm",
-            cost: 55
-        },
-        {
-            name: "Macro-gatling Blaster",
-            type: "Arm",
-            cost: 30
-        },
-        {
-            name: "Mori Quake Cannon",
-            type: "Arm",
-            cost: 20
-        },
-        {
-            name: "Sunfury Plasma Annihilator",
-            type: "Arm",
-            cost: 45
-        }],
-    reaver: [
-        {
-            name: "Volcano Cannon",
-            type: "Arm",
-            cost: 25
-        },
-        {
-            name: "Turbo Laser Destructor",
-            type: "Carapace",
-            cost: 20
-        },
-        {
-            name: "Melta Cannon",
-            type: "Arm",
-            cost: 35
-        },
-        {
-            name: "Apocalypse Missile Launcher",
-            type: "Carapace",
-            cost: 10
-        },
-        {
-            name: "Gatling Blaster",
-            type: "Arm",
-            cost: 15
-        },
-        {
-            name: "Vulcain Mega-bolter",
-            type: "Carapace",
-            cost: 10
-        },
-        {
-            name: "Reaver Titan Power Fist",
-            type: "Arm",
-            cost: 20
-        },
-        {
-            name: "Laser Blaster",
-            type: "Arm",
-            cost: 25
-        },
-        {
-            name: "Reaver Titan Chainfist",
-            type: "Arm",
-            cost: 20
-        },
-        {
-            name: "Warp Missible Support Rack",
-            type: "Carapace",
-            cost: 10
-        }
-    ],
-    warhaund: [
-        {
-            name: "Turbo Laser Destructor",
-            type: "Arm",
-            cost: 20
-        },
-        {
-            name: "Inferno Gun",
-            type: "Arm",
-            cost: 20
-        },
-        {
-            name: "Vulcain Mega-bolter",
-            type: "Arm",
-            cost: 10
-        }
-    ]
+    warlord: {
+        arm: [
+            {
+                name: "Plasma Blastgun",
+                type: "Arm",
+                cost: 30
+            },
+            {
+                name: "Arioch Titan Power Claw",
+                type: "Arm",
+                cost: 25
+            },
+            {
+                name: "Belicosa Volcano Cannon",
+                type: "Arm",
+                cost: 55
+            },
+            {
+                name: "Macro-gatling Blaster",
+                type: "Arm",
+                cost: 30
+            },
+            {
+                name: "Mori Quake Cannon",
+                type: "Arm",
+                cost: 20
+            },
+            {
+                name: "Sunfury Plasma Annihilator",
+                type: "Arm",
+                cost: 45
+            }
+        ],
+        carapace: [
+            {
+                name: "Apocalypse Missile Launchers",
+                type: "Carapace",
+                cost: 15
+            },
+            {
+                name: "Paired Gatling Blasters",
+                type: "Carapace",
+                cost: 30
+            },
+            {
+                name: "Paired Laser Blasters",
+                type: "Carapace",
+                cost: 50
+            },
+            {
+                name: "Paired Turbo Laser Destructors",
+                type: "Carapace",
+                cost: 35
+            },
+            {
+                name: "Vulcan Mega-bolter Array",
+                type: "Carapace",
+                cost: 20
+            },
+        ]   
+    },
+    reaver: {
+        arm: [
+            {
+                name: "Volcano Cannon",
+                type: "Arm",
+                cost: 25
+            },
+            {
+                name: "Melta Cannon",
+                type: "Arm",
+                cost: 35
+            },
+            {
+                name: "Reaver Titan Power Fist",
+                type: "Arm",
+                cost: 20
+            },
+            {
+                name: "Laser Blaster",
+                type: "Arm",
+                cost: 25
+            },
+            {
+                name: "Reaver Titan Chainfist",
+                type: "Arm",
+                cost: 20
+            },
+            {
+                name: "Gatling Blaster",
+                type: "Arm",
+                cost: 15
+            }
+        ],
+        carapace: [
+            {
+                name: "Turbo Laser Destructor",
+                type: "Carapace",
+                cost: 20
+            },
+            {
+                name: "Apocalypse Missile Launcher",
+                type: "Carapace",
+                cost: 10
+            },
+            ,
+            {
+                name: "Vulcain Mega-bolter",
+                type: "Carapace",
+                cost: 10
+            },
+            ,
+            {
+                name: "Warp Missible Support Rack",
+                type: "Carapace",
+                cost: 10
+            }
+        ]
+    },
+    warhound: {
+        arm: [
+            {
+                name: "Turbo Laser Destructor",
+                type: "Arm",
+                cost: 20
+            },
+            {
+                name: "Inferno Gun",
+                type: "Arm",
+                cost: 20
+            },
+            {
+                name: "Vulcain Mega-bolter",
+                type: "Arm",
+                cost: 10
+            }
+        ]
+    }
 };
 
 const titans = [
     {
         name: "Warlord",
+        type: "warlord",
         cost: 385,
         rightArm: null,
         leftArm: null,
@@ -136,6 +150,7 @@ const titans = [
     },
     {
         name: "Reaver",
+        type: "reaver",
         cost: 250,
         rightArm: null,
         leftArm: null,
@@ -143,12 +158,14 @@ const titans = [
     },
     {
         name: "Warhound",
+        type: "warhound",
         cost: 180,
         rightArm: null,
         leftArm: null
     },
     {
         name: "Cerastus Knight",
+        type: "cerastus",
         cost: 130,
         banner: true,
         rightArm: null,
@@ -156,6 +173,7 @@ const titans = [
     },
     {
         name: "Questoris Knight",
+        type: "questoris",
         cost: 120,
         banner: true,
         rightArm: null,
@@ -163,6 +181,7 @@ const titans = [
     },
     {
         name: "Acastus Knight",
+        type: "acastus",
         cost: 80,
         banner: true,
         rightArm: null,
--- js/at.js
+++ js/at.js
@@ -13,7 +13,13 @@ let loadVueApp = () => {
             total: 0,
             armyList: [],
             weapons: weapons,
-            titans: titans
+            titans: titans,
+            weaponModal: {
+                titanIndex: -1,
+                title: "",
+                titanWeaponAtt: "",
+                weapons: []
+            }
         },
         methods: {
             addTitan: function (titan) {
@@ -47,6 +53,29 @@ let loadVueApp = () => {
                 this.armyList.splice(index + 1, 0, titan);
                 this.total += titan.cost;
                 tooltipDispose();
+            },
+            openWeaponModal: function (index, titan, weaponType, titanWeaponAtt, modalTitle) {
+                this.weaponModal.titanIndex = index;
+                this.weaponModal.title = modalTitle;
+                this.weaponModal.titanWeaponAtt = titanWeaponAtt;
+
+                if(this.weapons[titan.type]){
+                    this.weaponModal.weapons = this.weapons[titan.type][weaponType];
+                    $('#weaponModal').modal('toggle');
+                }
+            },
+            chooseWeapon: function (weapon) {
+                this.weaponModal.weapons = [];
+                this.weaponModal.title = "";
+                let titan = this.armyList[this.weaponModal.titanIndex];
+                titan[this.weaponModal.titanWeaponAtt] = weapon;
+
+                console.log(titan);
+
+                this.armyList.splice(this.weaponModal.titanIndex, 1, titan);
+                this.weaponModal.titanIndex = -1;
+                this.weaponModal.weaponAtt = "";
+                $('#weaponModal').modal('toggle');
             }
         },
         updated: function () {
